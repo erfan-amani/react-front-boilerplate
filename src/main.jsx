bf7e031e3e4eec5b50ca173563e60f70765cb195
Erfan AmanAbadi <erfan80amani23@gmail.com>
2023-01-16 10:25:31 +0330

    added axios and enviroment variables

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,11 @@ import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistor, store } from "./store/store";
 import App from "./App";
+import setupAxios from "./library/setupAxios";
+import axios from "./library/http";
 import "./locales/index";
+
+setupAxios(axios, store);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
